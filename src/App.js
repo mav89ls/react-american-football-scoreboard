55431d6c6afe03c2d40e59d3474baf59634dfbd2
Marco Vargas <mav89ls@gmail.com>
2019-07-24 19:15:37 -0700

    set the hardcoded scores to the scores defined by the state, step 3 finished

--- src/App.js
+++ src/App.js
@@ -20,12 +20,12 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{hScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{aScore}</div>
           </div>
         </div>
         <BottomRow />
